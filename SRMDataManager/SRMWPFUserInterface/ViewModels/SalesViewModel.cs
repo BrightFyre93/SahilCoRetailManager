255baf5b2881d1c386cf0b0ed3924f0fbec523b9
Sahil Syal <sahil.syal@snicsolutions.com>
2021-12-30 10:43:40 +0530

    Get AddToCart working
    Get AddToCart working

--- SRMDataManager/SRMWPFUserInterface/ViewModels/SalesViewModel.cs
+++ SRMDataManager/SRMWPFUserInterface/ViewModels/SalesViewModel.cs
@@ -2,6 +2,7 @@
 using SRMDesktopUI.Library.API;
 using SRMDesktopUI.Library.Models;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SRMDesktopUI.ViewModels
@@ -39,9 +40,23 @@ namespace SRMDesktopUI.ViewModels
             }
         }
 
-        private BindingList<string> _cart;
+        private ProductModel _selectedProduct;
 
-        public BindingList<string> Cart
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -51,7 +66,7 @@ namespace SRMDesktopUI.ViewModels
             }
         }
 
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -60,6 +75,7 @@ namespace SRMDesktopUI.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -68,7 +84,12 @@ namespace SRMDesktopUI.ViewModels
             //TODO replace with calculation
             get
             {
-                return "$0.00";
+                decimal subTotal = 0;
+                foreach(var item in _cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+                return subTotal.ToString("C");
             }
         }
 
@@ -96,16 +117,41 @@ namespace SRMDesktopUI.ViewModels
             {
                 bool output = false;
 
-                // Make sure something is selected
-                // Make sure item quantity is greater than 0
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
 
                 return output;
             }
         }
 
-        public void AddToCard()
+        public void AddToCart()
         {
+            CartItemModel existingItem = Cart.FirstOrDefault(i => i.Product == SelectedProduct);
 
+            if(existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                // HACK there should be a better way
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel()
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+
+                Cart.Add(item);
+            }
+
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+            NotifyOfPropertyChange(() => Cart);
         }
 
         public bool CanRemoveFromCart
