37c271b8ea2234e0e8f05357a3d5747050fcc5ba
Sahil Syal <sahil.syal@snicsolutions.com>
2021-12-31 12:21:11 +0530

    replace container reference in shellviewmodel
    replace container reference in shellviewmodel

--- SRMDataManager/SRMWPFUserInterface/ViewModels/ShellViewModel.cs
+++ SRMDataManager/SRMWPFUserInterface/ViewModels/ShellViewModel.cs
@@ -7,18 +7,15 @@ namespace SRMDesktopUI.ViewModels
     {
         private readonly IEventAggregator _events;
         private readonly SalesViewModel _salesVM;
-        private readonly SimpleContainer _container;
 
         public ShellViewModel(IEventAggregator events,
-            SalesViewModel salesVM,
-            SimpleContainer container)
+            SalesViewModel salesVM)
         {
             _events = events;
             _salesVM = salesVM;
-            _container = container;
 
             _events.Subscribe(this);
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
