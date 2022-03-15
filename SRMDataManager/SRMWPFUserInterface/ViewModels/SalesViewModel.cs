7041edf1fd7c3a515278c61abe19819e747275a5
Sahil Syal <sahil.syal@snicsolutions.com>
2022-03-15 12:40:29 +0530

    Added RemoveFromCart functionality
    Added RemoveFromCart functionality

--- SRMDataManager/SRMWPFUserInterface/ViewModels/SalesViewModel.cs
+++ SRMDataManager/SRMWPFUserInterface/ViewModels/SalesViewModel.cs
@@ -64,6 +64,18 @@ namespace SRMDesktopUI.ViewModels
             }
         }
 
+        private CartItemDisplayModel _selectedCartItem;
+
+        public CartItemDisplayModel SelectedCartItem
+        {
+            get { return _selectedCartItem; }
+            set
+            {
+                _selectedCartItem = value;
+                NotifyOfPropertyChange(() => SelectedCartItem);
+                NotifyOfPropertyChange(() => CanRemoveFromCart);
+            }
+        }
 
         private BindingList<CartItemDisplayModel> _cart = new BindingList<CartItemDisplayModel>();
 
@@ -183,7 +195,10 @@ namespace SRMDesktopUI.ViewModels
             {
                 bool output = false;
 
-                // Make sure something is selected
+                if (SelectedCartItem != null && SelectedCartItem?.Product.QuantityInStock > 0)
+                {
+                    output = true;
+                }
 
                 return output;
             }
@@ -191,6 +206,16 @@ namespace SRMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
+            SelectedCartItem.Product.QuantityInStock += 1;
+
+            if (SelectedCartItem.QuantityInCart > 1)
+            {
+                SelectedCartItem.QuantityInCart -= 1;
+            }
+            else
+            {
+                Cart.Remove(SelectedCartItem);
+            }
 
             NotifyOfPropertyChange(() => SubTotal);
             NotifyOfPropertyChange(() => Tax);
